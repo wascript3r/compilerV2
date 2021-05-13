f3c0ea9647cad49307fd3d3b63372be1ea8e452c
WaScript3r <wascript3r@gmail.com>
2021-05-13 15:49:49 +0300

    shell.py -> run.py

---
+++ run.py
@@ -0,0 +1,18 @@
+import basic
+
+result, err = basic.run('<stdin>', 'RUN("example.myopl")')
+if err:
+    print(err.as_string())
+
+# while True:
+# 	text = input('basic > ')
+# 	if text.strip() == "": continue
+# 	result, error = basic.run('<stdin>', text)
+#
+# 	if error:
+# 		print(error.as_string())
+# 	elif result:
+# 		if len(result.elements) == 1:
+# 			print(repr(result.elements[0]))
+# 		else:
+# 			print(repr(result))
